8abfc0ad8cb16e83f82565e83d671e9fb91f78e1
akash <flexdeploy@domain.com>
2024-07-01 11:32:24 -0500

    .js Change

--- FlexDeploy 8.0.0.4/fdtlt87/lwc/activeAccount/activeAccount.js
+++ FlexDeploy 8.0.0.4/fdtlt87/lwc/activeAccount/activeAccount.js
@@ -2,6 +2,7 @@ import { LightningElement, wire } from 'lwc';
 import getActiveAccounts from '@salesforce/apex/AccountController.getActiveAccounts';
 
 export default class ActiveAccount extends LightningElement {
+    // Change 1.0
     @wire(getActiveAccounts)
     accounts;
 }
