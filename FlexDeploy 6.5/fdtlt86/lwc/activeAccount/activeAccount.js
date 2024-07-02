43c074d425bb4bd1a26dd98f651ba0ccd9365ea1
Akash Bangalkar <akash.bangalkar@flexagon.com>
2024-07-02 01:48:34 -0500

    LWC, Apex and Aura

---
+++ FlexDeploy 6.5/fdtlt86/lwc/activeAccount/activeAccount.js
@@ -0,0 +1,9 @@
+import { LightningElement, wire } from 'lwc';
+import getActiveAccounts from '@salesforce/apex/AccountController.getActiveAccounts';
+
+export default class ActiveAccount extends LightningElement {
+    // Change 1.0
+    // Change 2.0
+    @wire(getActiveAccounts)
+    accounts;
+}
