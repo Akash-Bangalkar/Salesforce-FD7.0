d8d532f238d2fbbd856a8cce2bbd5691cb26aa57
akash <flexdeploy@domain.com>
2024-07-02 06:09:27 -0500

    LWC - .js

--- FlexDeploy 9.0/fdtlt82/lwc/activeAccount/activeAccount.js
+++ FlexDeploy 9.0/fdtlt82/lwc/activeAccount/activeAccount.js
@@ -5,6 +5,7 @@ export default class ActiveAccount extends LightningElement {
     // Change 1.0
     // Change 2.0
     // Change 6.5
+    // Change 9.0
     @wire(getActiveAccounts)
     accounts;
 }
